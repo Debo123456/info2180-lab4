3552860c2fd48046cd576d7682d339a8d560a099
Daniel Dawson <Daniel Dawson>
2016-10-13 00:46:43 -0500

    Alerts on succesful completion of maze

--- maze.js
+++ maze.js
@@ -3,22 +3,32 @@ window.onload = function main()
 {
 	
 	var $wall = document.getElementsByClassName("boundary");
-		
+	var $end = document.getElementById("end");	
+	var touchedWall = false;
 		
 		
 		for(var i = 0; i < $wall.length; i++)
 		{
-			$wall.item(i).addEventListener("mouseover", function lose()
-		{
-			for(var i = 0; i < $wall.length; i++)
-			{		
-				$wall.item(i).setAttribute("class", "boundary youlose");
-			}
-		} );
-		
-		
-			
+			$wall.item(i).addEventListener("mouseover", function()
+			{
+				for(var i = 0; i < $wall.length; i++)
+				{		
+					$wall.item(i).setAttribute("class", "boundary youlose");
+				}
+			});
 		}
+		
+		$end.addEventListener("mouseover", function()
+		{
+			if(touchedWall === false)
+			{
+				alert("Succesful completion of maze");
+			}
+			else
+			{
+				alert("Unsuccesful, try again");
+			}
+		});
 		
 }
 
