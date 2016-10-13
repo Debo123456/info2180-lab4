b662cd8353d32718407f2910a7a52a2f0a89703b
Daniel Dawson <Daniel Dawson>
2016-10-13 02:05:36 -0500

    Code enhancement, boundary can only turn red after 'S' is clicked and not befor the game is started

--- maze.js
+++ maze.js
@@ -7,42 +7,75 @@ window.onload = function main()
 	var $start = document.getElementById("start");	
 	var touchedWall = false;
 	var $status = document.getElementById("status");
+	var $maze = document.getElementById("maze");
 		
+	var youLose = function($wall)
+	{
+		touchedWall = true;
+		for(var i = 0; i < $wall.length; i++)
+		{		
+			$wall.item(i).setAttribute("class", "boundary youlose");
+		}
+	}
+	
+	var start = function(evt)
+	{
+		touchedWall = false;
+		console.log(touchedWall);
+		for(var i = 0; i < $wall.length; i++)
+		{		
+			$wall.item(i).setAttribute("class", "boundary");
+		}
 		
 		for(var i = 0; i < $wall.length; i++)
 		{
 			$wall.item(i).addEventListener("mouseover", function()
 			{
-				touchedWall = true;
-				for(var i = 0; i < $wall.length; i++)
-				{		
-					$wall.item(i).setAttribute("class", "boundary youlose");
-				}
+				youLose($wall);
 			});
 		}
 		
+		document.addEventListener("mouseover", function(){
+			if(event.clientX < $maze.offsetLeft || event.clientX > $maze.offsetRight)
+			{
+				youLose($wall);
+			}
+		});
+	}
+	
+	var end = function()
+	{
+		console.log(touchedWall);
+		if(touchedWall == false)
+		{
+			$status.innerHTML = "Succesful completion of maze";
+			$status.style.color = "green";
+		}
+		else
+		{
+			$status.innerHTML = "You lost, try again";
+			$status.style.color = "red";
+		}
+	}
+	
+	
+	
+	
+	var game = function()
+	{
+		
+		
 		$start.addEventListener("click", function()
 		{
-			for(var i = 0; i < $wall.length; i++)
-			{		
-				$wall.item(i).setAttribute("class", "boundary");
-			}
-			touchedWall = false;
+			start();
 		});
 		
 		$end.addEventListener("mouseover", function()
 		{
-			if(touchedWall === false)
-			{
-				$status.innerHTML = "Succesful completion of maze";
-				$status.style.color = "green";
-			}
-			else
-			{
-				$status.innerHTML = "You lost, try again";
-				$status.style.color = "red";
-			}
+			end();
 		});
-		
+	}
+	
+	game();
 }
 
